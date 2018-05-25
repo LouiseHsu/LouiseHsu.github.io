9b72ccdb6dd02607fce62eb414c7f2ccde67b0e7
Louise Hsu <louiseh0313@yahoo.ca>
2018-05-25 02:21:13 -0700

    Made auto scroll consistent, added arrow, added projects section

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,41 @@
 $(document).ready(function() {
-
+  var  pgSection = $(window).height()-70;
+/*
   $("a").click(function(e) {
     var linkHref = $(this).attr('href');
-    
+    console.log($(linkHref).offset().top - 70);
+    console.log($(linkHref));
     $('html, body').animate({
-      scrollTop: $(linkHref).offset().top
+
+      scrollTop: ($(linkHref).offset().top - 70)
     });
 
     e.preventDefault();
     
   });
+  */
+ $("#title").click(function() {
+  console.log("clicked");
+  $('html, body').animate({
+    scrollTop: (0)
+  });
+});
+
+ $("#aboutnav, #introarrow").click(function() {
+  console.log("clicked");
+  $('html, body').animate({
+    scrollTop: ($(window).height()-70)
+  });
+});
+
+$("#projectsnav, #aboutarrow").click(function() {
+  console.log("clicked");
+  $('html, body').animate({
+    scrollTop: (pgSection * 2)
+  });
+});
+
+
 
   function bounceArrow() {
     for(i=0; i<10; i++) {
